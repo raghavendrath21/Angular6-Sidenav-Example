6ac6827e3af33c952df73400451ebd40b3231a84
raghavendra <raghavendrath21@gmail.com>
2019-11-08 14:38:07 +0530

    Add new changes files for Angular Sidenav Example

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { SecondComponent } from "./second/second.component";
+import { FirstComponent } from "./first/first.component";
+import { RouterModule, Routes } from "@angular/router";
+import { CustomMaterialModule } from "./core/material.module";
+
+const appRoutes: Routes = [
+  { path: "", component: FirstComponent, data: { title: "First Component" } },
+  { path: "first", component: FirstComponent, data: { title: "First Component" } },
+  { path: "second", component: SecondComponent, data: { title: "Second Component" } }
+];
+@NgModule({
+  declarations: [AppComponent, NavigationComponent, SecondComponent, FirstComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { useHash: true } // <-- debugging purposes only
+    ),
+    CustomMaterialModule
+  ],
+  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
